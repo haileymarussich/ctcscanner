bd920f470073fd180ae1d6787157dd39eb14b8d2
haileymarussich <hailey617@gmail.com>
2022-09-26 15:27:03 -0400

    make it better

--- CTCscanner.py
+++ CTCscanner.py
@@ -48,7 +48,7 @@ with st.sidebar:
     selected = option_menu(
         menu_title=None,
         options = ['Customer Search', 
-                   'Aggregate Volumes', 
+                   'Agg. Volumes', 
                    'Shared Wallet Scanner',
                    'OFAC Matches',
                    'Transaction Log', 'Customer Database', 'All Data'])
@@ -86,7 +86,6 @@ tx_log_df = pd.DataFrame(tx_data[1:], columns=tx_data[0])
 
 # Datetime (TX_Log)
 tx_log_df['TX_Date'] = pd.to_datetime(tx_log_df['TX_Date'])
-tx_log_df['TX_Date'] = tx_log_df['TX_Date'].apply(lambda x: x.replace(hour=0, minute=0, second=0, microsecond=0))
 
 
 
@@ -131,34 +130,17 @@ BL_addresses = pd.DataFrame(BL_data[1:], columns=BL_data[0])
 
 
 
-# Check DF info
-print(customer_database_df.dtypes)
-print(customer_database_df.shape)
-
-print(tx_log_df.dtypes)
-print(tx_log_df.shape)
-
-print(SAR_log.dtypes)
-print(SAR_log.shape)
-
-print(BL_addresses.dtypes)
-print(BL_addresses.shape)
-
-
-
 # CONCAT SHEETS
 concat_df1 = pd.merge(tx_log_df, customer_database_df, on = "ID", how = "outer", indicator="indicator_column")
 concat_df = pd.merge(concat_df1, SAR_log, on = "SAR_ID", how = "outer", indicator="indicator_column2")
 
 concat_df.columns = concat_df.columns.str.replace(' ', '')
-
 concat_df["Control"] = pd.to_numeric(concat_df["Control"], errors="coerce")
 concat_df = concat_df.sort_values(by='Control', ascending=True)
-
 concat_df = concat_df.replace('', np.nan, regex=True)
 
 
-# Date last transacted (Concat_Df)
+# Date last transacted (Concat_DF)
 concat_df['Last_TX'] = concat_df.groupby('ID_x')['TX_Date'].transform('max')
 concat_df['Last_TX'] = pd.to_datetime(concat_df['Last_TX']).dt.date
 
@@ -172,7 +154,6 @@ concat_df['Agg_Volume'] = concat_df.groupby('ID_x')['Trade_Value'].transform(sum
 # Shared wallets (Concat_DF) and new DF
 shared_wallets_df = concat_df.groupby(['Address']).agg(
         Shared_Count = ('ID_x', 'nunique'), Shared_Identities = ('ID_x', 'unique'), Shared_Total = ('Trade_Value', 'sum'), Wallet_Last_TX = ('TX_Date', 'max'))
-    
 shared_wallets_df = shared_wallets_df.loc[(shared_wallets_df['Shared_Count'] > 1)]
 
 shared_wallets_df['Wallet_Last_TX'] = pd.to_datetime(shared_wallets_df['Wallet_Last_TX']).dt.date
@@ -180,13 +161,6 @@ shared_wallets_df = shared_wallets_df.sort_values(by='Wallet_Last_TX', ascending
 
 concat_df = pd.merge(concat_df, shared_wallets_df, on = "Address", how = "outer")
 concat_df['Wallet_Last_TX'] = pd.to_datetime(concat_df['Wallet_Last_TX']).dt.date
-
-
-# Duplicate phone numbers (Concat_DF)
-#duplicate_phone_df = concat_df.groupby(['Phone']).agg(
-#        Count = ('ID_x', 'nunique'), Identities = ('ID_x', 'unique'), Phone_Shared_Total = ('Trade_Value', 'sum'), Phone_Last_TX = ('TX_Date', 'max'))
-#
-#duplicate_phone_df = duplicate_phone_df.loc[(duplicate_phone_df['Count'] > 1)]
 
 
 # High-risk customers (Concat_DF)
@@ -200,11 +174,6 @@ concat_df['Percentile'] = concat_df.Agg_Volume.rank(pct = True)
 concat_df.loc[concat_df['Percentile'] > .9, 'Risk_Rating'] = "High Volume"
 
 concat_df.loc[concat_df['Company_Type'] == "Financial Institution", 'Risk_Rating'] = "Financial Institution"
-
-# Screening result (Concat_DF)
-#list_addresses = BL_addresses['Address'].tolist()
-#bl_addresses_list = BL_addresses['Address'].unique()  
-#concat_df['Address_Match'] = np.where(concat_df['Address'].isin(bl_addresses_list), "Yes", "No")
 
 concat_df['Address'].fillna("Missing", inplace=True)
 concat_df['Address_Match'] = concat_df.Address.isin(BL_addresses.Address)
@@ -222,7 +191,6 @@ status_options = concat_df.Risk_Rating.unique()
 # Dateindex (Concat_DF)
 dateindex_concat = concat_df.set_index('TX_Date') 
 
-
 # Start and end date (Concat_DF)
 min_start_date = dateindex_concat.index.min()
 max_end_date = dateindex_concat.index.max()
@@ -236,7 +204,6 @@ concat_df.loc[(concat_df['Risk_Rating'].isin(high_risk)) &
 
 concat_df.loc[(concat_df['Status'] == "Dormant"), 
               'Review_Needed'] = 'Yes'
-#concat_df['Review_Needed'].fillna(" ", inplace=True)
 
 
 # Statements needed (Concat_DF)
@@ -318,9 +285,9 @@ if selected == 'Customer Database':
 
         
 # AGGREGATE TOTALS
-if selected == 'Aggregate Volumes':
+if selected == 'Agg. Volumes':
 # Tile and file names
-    st.title('Aggregate Volumes')
+    st.title('Agg. Volumes')
         
 # Frame
     agg_from_concat = concat_df[['Agg_Volume', 'Percentile', 'ID_x', 'Name_y', 'Company_Name', 'Last_TX', 'Status', 'Risk_Rating',
@@ -328,6 +295,7 @@ if selected == 'Aggregate Volumes':
                          
     agg_from_concat.drop_duplicates(subset=['ID_x'], inplace=True)
     agg_from_concat.set_index('ID_x', inplace=True)
+    agg_from_concat = agg_from_concat.sort_values(by='Agg_Volume', ascending=True)
         
 # Print
     st.write(agg_from_concat.shape)  
@@ -442,6 +410,7 @@ if selected == 'All Data':
 
             if is_datetime64_any_dtype(df[col]):
                 df[col] = df[col].dt.tz_localize(None)
+                df[col] = df[col].dt.date
 
         modification_container = st.container()
 
@@ -525,3 +494,16 @@ if selected == 'Customer Search':
     
 # Tile and file names
     st.title('Customer Search')
+    
+# Filter
+    with st.form("my_form"):
+        user_text_input = st.text_input("Substring or regex in Name")
+        user_text_input = st.text_input("Substring or regex in ID")
+        submit = st.form_submit_button("Submit")
+        
+    if submit:
+        concat_df = concat_df[concat_df["Name_y"].astype(str).str.contains(user_text_input)]
+        concat_df = concat_df[concat_df["ID_x"].astype(str).str.contains(user_text_input)]
+        
+        st.write(concat_df.shape)
+        st.dataframe(concat_df)
