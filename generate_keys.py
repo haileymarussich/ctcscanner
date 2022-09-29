68470028dcd89019d1d6f988dcf36ba304c2fb96
haileymarussich <hailey617@gmail.com>
2022-09-29 13:12:28 -0400

    make it better

---
+++ generate_keys.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 29 11:53:32 2022
+
+@author: haile
+"""
+
+import pickle
+from pathlib import Path
+
+import streamlit_authenticator as stauth
+
+names = ["Glenn Hay-Roe", "Jesse Parsons", "Hailey Marussich"]
+usernames = ["ghayroe", "jparsons", "hmarussich"]
+passwords = ["ghctcservice1776!", "jpctcservice1776!", "hmctcservice1776!"]
+
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+file_path = Path(__file__).parent / "hashed_pw.pk1"
+with file_path.open("wb") as file:
+    pickle.dump(hashed_passwords, file)
